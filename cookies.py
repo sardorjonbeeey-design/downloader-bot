a7be462e2e536a999af068011294e9e1595dcc81
sardorjonbeeey-design <sardorjonbeeey@gmail.com>
2026-07-20 14:05:40 +0300

    Create cookies.py

---
+++ cookies.py
@@ -0,0 +1,24 @@
+# Yukla Pro - YouTube/TikTok Cookies
+# 
+# Since I'm on mobile, I'll add real cookies later.
+# The bot will show clear error messages instead of crashing.
+# 
+# To add real cookies later:
+# 1. Use a computer
+# 2. Install Chrome extension "Get cookies.txt LOCALLY"
+# 3. Go to youtube.com and login
+# 4. Export cookies
+# 5. Replace this file with the real cookies
+#
+# For now, YouTube and Music Search will show:
+# "❌ YouTube Authentication Required"
+#
+# Instagram and TikTok will still work.
+
+# Placeholder cookies
+# Replace this whole file with real cookies from YouTube
+
+# Real cookies should look like this:
+# .youtube.com	TRUE	/	TRUE	1760000000	VISITOR_INFO1_LIVE	your_value_here
+# .youtube.com	TRUE	/	TRUE	1760000000	CONSENT	YES+UZ
+# .youtube.com	TRUE	/	TRUE	1760000000	PREF	f1=50000000
