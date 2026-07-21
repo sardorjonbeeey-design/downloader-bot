362dcac22b2aa4e7e3efc83a9958224526451331
sardorjonbeeey-design <sardorjonbeeey@gmail.com>
2026-07-21 15:57:56 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,15 +1,10 @@
 """
-Handlers - Export all handlers
+Handlers - Export all handlers from their respective files
 """
-from handlers.main_handler import (
-    start_command,
-    help_command,
-    info_command,
-    handle_url,
-    handle_music_callback,
-)
+from handlers.start import start_command, help_command, info_command
+from handlers.download import handle_url
 from handlers.youtube import handle_youtube, youtube_callback_handler
-from handlers.music import handle_music_search
+from handlers.music import handle_music_search, handle_music_callback
 from handlers.admin import stats_command, reset_stats
 
 __all__ = [
@@ -17,7 +12,8 @@ __all__ = [
     'help_command',
     'info_command',
     'handle_url',
-    'handle_youtube_callback',
+    'handle_youtube',
+    'youtube_callback_handler',
     'handle_music_search',
     'handle_music_callback',
     'stats_command',
